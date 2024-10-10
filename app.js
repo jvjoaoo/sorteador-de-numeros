16736ccb467da8adde2b4defd749d7445e7d909d
jvjoaoo <jvjoaoo.vitor@gmail.com>
2024-10-09 21:36:57 -0300

    Inserindo o texto na tela com innerHTML

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ function sortear() {
   for (let i = 0; i < quantidade; i++) {
     numero = obterNumeroAleatorio(de, ate)
 
+    while (sorteados.includes(numero)) {
+      numero = obterNumeroAleatorio(de, ate)
+    }
+
     sorteados.push(numero)
   }
 
-  console.log(sorteados)
+  let resultado = document.getElementById("resultado")
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
 }
 
 function obterNumeroAleatorio(min, max) {
